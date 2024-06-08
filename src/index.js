a757e808154ee6298ffcd6ee54cc30d0ff3d5c2f
Nikita Teslov <nteslov02@gmail.com>
2024-06-08 22:55:13 +0600

    ghjkl

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import UserStore from "./store/UserStore";
+import CarStore from './store/CarStore';
+import ServiceStore from './store/ServiceStore';
 
+export const Context = createContext(null)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Context.Provider value={{user: new UserStore(), cars: new CarStore(), services: new ServiceStore()}}>
+    <App />
+  </Context.Provider>
+);
+
+/*
+ReactDOM.render(
+  <Context.Provider value={{
+      user: new UserStore(),
+  }}>
+      <App />
+  </Context.Provider>,
+document.getElementById('root')
+);*/
+
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);*/
